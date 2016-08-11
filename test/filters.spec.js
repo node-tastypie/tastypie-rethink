ba0d3882c0e13d00597137360ad4ea3bbcf333e9
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-08-10 21:08:23 -0500

    New filter tests for month day and year filters

--- test/filters.spec.js
+++ test/filters.spec.js
@@ -57,13 +57,16 @@ var queryset, Rethink;
 describe('rethink', function(){
 
 	before(function( done ){
-		Model.save( JSON.parse( fs.readFileSync( path.resolve(__dirname,'./data/test.json') ) ) ).then(function( records ){
-			done();
-		})
+		Model.delete()
+			.then(function(){
+				Model.save( JSON.parse( fs.readFileSync( path.resolve(__dirname,'./data/test.json') ) ) ).then(function( records ){
+					done();
+				})
+			})
 	});
 
 	after(function( done ){
-		done();
+		Model.delete().then(( )=>{done()});
 	});
 
 
@@ -453,6 +456,325 @@ describe('rethink', function(){
 					})
 					.catch( done )
 			})
+		});
+
+		describe('day', function( ){
+			describe('~monday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'monday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(1);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+			describe('~tuesday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'tuesday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(2);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+			describe('~wednesday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'wednesday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(3);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+			describe('~thursday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'thursday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(4);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+			describe('~friday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'friday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(5);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+			describe('~saturday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'saturday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(6);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+			describe('~sunday',function(){
+				it('should only return data on a specific day', function(done){
+					Model.filter( filters.day( Model.r, 'registered', 'sunday'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCDay().should.equal(0);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+
+		});
+
+		describe('month', function(){
+			describe('~janurary',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'january'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(0);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~February',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'february'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(1);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~march',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'march'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(2);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~april',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'april'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(3);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~may',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'may'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(4);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~june',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'june'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(5);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~july',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'july'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(6);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~august',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'august'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(7);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~september',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'september'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(8);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~october',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'october'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(9);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~november',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'november'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(10);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+			describe('~december',function(){
+				it('should only return data on a specific month', function(done){
+					Model.filter( filters.month( Model.r, 'registered', 'december'))
+						.limit(10)
+						.then(function( data ){
+							data.length.should.be.above(0)
+							for(var d of data){
+								d.registered.getUTCMonth().should.equal(11);
+							}
+							done()
+						})
+						.catch( done );
+				});
+			});
+		});
+
+		describe('year', function(){
+			it('should only return data during 2014', function(done){
+				Model.filter( filters.year( Model.r, 'registered', 2014))
+					.limit(10)
+					.then(function( data ){
+						data.length.should.be.above(0)
+						for(var d of data){
+							d.registered.getUTCFullYear().should.equal(2014);
+						}
+						done()
+					})
+					.catch( done );
+			});
+			it('should only return data during 2015', function(done){
+				Model.filter( filters.year( Model.r, 'registered', 2015))
+					.limit(10)
+					.then(function( data ){
+						data.length.should.be.above(0)
+						for(var d of data){
+							d.registered.getUTCFullYear().should.equal(2015);
+						}
+						done()
+					})
+					.catch( done );
+			});
+
+			it('should only return data during 2016', function(done){
+				Model.filter( filters.year( Model.r, 'registered', 2016))
+					.limit(10)
+					.then(function( data ){
+						data.length.should.be.above(0)
+						for(var d of data){
+							d.registered.getUTCFullYear().should.equal(2016);
+						}
+						done()
+					})
+					.catch( done );
+			});
 		});
 	});
 
